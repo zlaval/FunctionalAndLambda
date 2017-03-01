3f9e5439bf96a0009b6246b811ca95c2ba4e140b
Zalan Toth <zlaval@gmail.com>
2017-03-02 00:12:40 +0100

    predicate added

--- src/com/zlrx/builtin/BuiltInFuncionalInterfaces.java
+++ src/com/zlrx/builtin/BuiltInFuncionalInterfaces.java
@@ -1,5 +1,6 @@
 package com.zlrx.builtin;
 
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
@@ -16,8 +17,33 @@ public class BuiltInFuncionalInterfaces {
                 IntStream.range(0, 100)
                         .filter(i -> i % 2 == 0)
                         .map(i -> i * 10);
+    }
+
+    private void predicateExample() {
+        Predicate<Integer> evenIntPredicate = x -> (x & 1) == 0;
+        evenIntPredicate.test(10);
+        evenIntPredicate.test(11);
+
+        Predicate<Integer> oddIntPredicate = evenIntPredicate.negate();
+        oddIntPredicate.test(10);
+        oddIntPredicate.test(11);
+    }
+
+    private void functionExamle() {
 
     }
 
+    private void consumerExmaple() {
+
+    }
+
+    private void binaryOperatorExample() {
+
+    }
+
+    public static void main(String[] args) {
+        BuiltInFuncionalInterfaces builtInFuncionalInterfaces = new BuiltInFuncionalInterfaces();
+        builtInFuncionalInterfaces.predicateExample();
+    }
 
 }
